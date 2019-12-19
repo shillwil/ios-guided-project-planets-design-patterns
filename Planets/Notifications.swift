8c581418d70f9651bb4610d87e046203b9c46e0c
Alex Shillingford <alex.shillingford@icloud.com>
2019-12-19 10:39:38 -0700

    Create extension on Notification.Name

---
+++ Planets/Notifications.swift
@@ -0,0 +1,15 @@
+//
+//  Notifications.swift
+//  Planets
+//
+//  Created by Alex Shillingford on 12/19/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import Foundation
+
+
+extension Notification.Name {
+    static var plutoSwitchWasFlipped = Notification.Name("plutoSwitchWasFlipped")
+    
+}
